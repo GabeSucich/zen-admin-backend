f3b570ca6fa3b73ca553dfb3359a37832ed20524
Gabe Sucich <gabe.sucich@gmail.com>
2026-03-09 08:12:17 -0700

    Add Neon pinging

--- database.py
+++ database.py
@@ -4,7 +4,13 @@ from utils.env_vars import EnvVarName, load_env_var
 
 DATABASE_URL = load_env_var(EnvVarName.DATABASE_URL)
 
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10
+)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 async def get_db():
